65859d805fbbadf6f98b2a98dd96c9d0b82b3363
Nikita Merzlyakov <merzlyakov02@gmail.com>
2023-09-24 14:46:11 +0300

    added base components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import {Navigation} from "./components/nav/navigation";
 import {Login} from "./components/auth/login";
+import {Logout} from "./components/auth/logout";
+import {Home} from "./components/home";
+import {useState} from "react";
+import StudMarks from "./components/marks/professorToStudentMarksView";
 
 function App() {
+    const [user, setUser] = useState(null)
     return (
         <BrowserRouter>
-            <Navigation></Navigation>
+            <Navigation user={user} setUser={setUser}/>
             <Routes>
-                {/*<Route path="/" element={<Home/>}/>*/}
-                <Route path="/login" element={<Login/>}/>
-                {/*<Route path="/logout" element={<Logout/>}/>*/}
+                <Route path="/" element={<Home user={user} setUser={setUser}/>}/>
+                <Route path="/login" element={<Login setUser={setUser}/>}/>
+                <Route path="/logout" element={<Logout/>}/>
+                <Route path="/studmarks" element={<StudMarks/>}/>
             </Routes>
         </BrowserRouter>
     )
